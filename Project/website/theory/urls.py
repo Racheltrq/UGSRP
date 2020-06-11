9894bbf1053dd0137c0fb196f1bd7ebb267dbbcd
Rachel Tao <rt1726@nyu.edu>
2020-06-11 15:19:08 -0400

    fix url routing

--- Project/website/theory/urls.py
+++ Project/website/theory/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^theory/(\d+)/$', views.key_detail, name='key_detail'),
     url(r'^theory/(\d+)/common/', views.common, name='common'),
     url(r'^theory/(\d+)/notcommon/', views.notcommon, name='notcommon')
