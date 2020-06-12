4067fb67b2db1e3e383120d2adeb70b639c0fa59
Rachel Tao <rt1726@nyu.edu>
2020-06-12 01:21:38 -0400

    add most view test

--- Project/website/theory/tests/test_views.py
+++ Project/website/theory/tests/test_views.py
@@ -7,7 +7,12 @@ class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
         self.home = reverse('home')
-        
+        self.key_detail = reverse('key_detail', args=[1])
+        self.common = reverse('common', args=[1])
+        self.notcommon = reverse('notcommon', args=[1])
+        self.key1 = Key.objects.create(
+            name = 'D'
+        )
 
 
     def test_home_view(self):
@@ -16,3 +21,26 @@ class TestViews(TestCase):
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
+
+
+    def test_detail_view(self):
+
+        response = self.client.get(self.key_detail)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'key_detail.html')
+
+
+    def test_common_view(self):
+
+        response = self.client.get(self.common)
+
+        self.assertEquals(response.status_code, 302)
+
+
+
+    def test_notcommon_view(self):
+
+        response = self.client.get(self.notcommon)
+
+        self.assertEquals(response.status_code, 302)
