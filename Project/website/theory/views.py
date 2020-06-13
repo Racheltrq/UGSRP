9f22810e901428036f15719af5851f34f59134ce
Rachel Tao <rt1726@nyu.edu>
2020-06-12 21:01:13 -0400

    change hardcode to for loop

--- Project/website/theory/views.py
+++ Project/website/theory/views.py
@@ -14,28 +14,26 @@ def home(request):
 
 def get_files(key):
     if key.type == 'maj':
+        files = []
         midiFile = str(key).replace(" ", "") + ".midi"
         pngFile = str(key).replace(" ", "") + ".png"
+        files.append(midiFile)
+        files.append(pngFile)
     else:
-        midiFile = []
-        pngFile = []
-        midiFile.append(str(key).replace(" ", "") + "1.midi")
-        midiFile.append(str(key).replace(" ", "") + "2.midi")
-        midiFile.append(str(key).replace(" ", "") + "3.midi")
-        pngFile.append(str(key).replace(" ", "") + "1.png")
-        pngFile.append(str(key).replace(" ", "") + "2.png")
-        pngFile.append(str(key).replace(" ", "") + "3.png")
-    return midiFile, pngFile
+        files = []
+        files.append([str(key).replace(" ", "") + "1.midi", str(key).replace(" ", "") + "1.png"])
+        files.append([str(key).replace(" ", "") + "2.midi", str(key).replace(" ", "") + "2.png"])
+        files.append([str(key).replace(" ", "") + "3.midi", str(key).replace(" ", "") + "3.png"])
+    return files
 
 
 
 def key_detail(request, id):
     key = get_object_or_404(Key, id=id)
-    midiFile, pngFile = get_files(key)
+    files = get_files(key)
     return render(request, 'key_detail.html', {
                                                 'key': key,
-                                                'pngFile': pngFile,
-                                                'midiFile': midiFile
+                                                'file': files
                                                 })
 
 def toggle_common(request, id, state):
